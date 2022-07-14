5ee0ce9b9d3859f315c9cfe8ffca06bec5bbb4db
akhil <makhil006@gmail.com>
2022-07-14 12:09:33 +0530

    Update handler.lua

--- kong/plugins/custom-introspection/handler.lua
+++ kong/plugins/custom-introspection/handler.lua
@@ -333,8 +333,8 @@ local function do_authentication(conf)
 
     -- update upstream request body with bvn_data
     -- local encoded_bvn_data = ngx.encode_args(credential_obj.bvn_data)
-    -- kong.log.notice(credential_obj.bvn_data)
     local encoded_bvn_data = cjson.encode(credential_obj.bvn_data)
+    kong.log.notice(encoded_bvn_data)
     -- ngx.req.set_body_data(credential_obj.bvn_data)
     kong.service.request.set_raw_body(encoded_bvn_data)
 
